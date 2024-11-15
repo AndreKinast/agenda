c3a7acfec18c3e8ce739d7ba9b0f40c277156ebb
AndreKinast <kinast_andre@hotmail.com>
2024-11-15 20:42:46 -0300

    Fiz as migrações e criei models contact

--- comandos/readme.md
+++ comandos/readme.md
@@ -20,3 +20,17 @@ git config --global init.defaultBranch main
 
 #configure o gitignore
 git init
+
+````
+Migrando a base de dados do Django
+
+````
+python manage.py makemigrations
+python manage.py migrate
+````
+Criando e modificando a senha de um super usuário Django
+
+````
+python manage.py createsuperuser
+python manage.py changepassword USERNAME
+````
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+#ID (primary key)
+# first_name (string), last_name(string), phone(string)
+#email (email), created_date(date), description(text)
+#category(foreing key), show(boolean), owner(foreing key)
+#picture(imagem)
+
+
+#os campos de texto normalmente são obrigatórios, para que não seja é preciso adicionar 'blank=true'
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
